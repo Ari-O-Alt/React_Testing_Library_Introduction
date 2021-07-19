25195b4d8690fc5b8e68f42649fd14a035bdb5d7
Arika O <11184750+Ari-O-Alt@users.noreply.github.com>
2021-07-19 23:17:17 +0200

    Wrote more tests for the Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,12 @@
-import React from 'react'
-import "./Header.css"
+import React from "react";
+import "./Header.css";
 
-export default function Header({
-    title
-}) {
-    return <h1 className="header">{title}</h1>
+export default function Header({ title }) {
+  return (
+    <>
+      <h1 className="header">{title}</h1>
+      <h3>Header 3</h3>
+      <h2 title={"Target This"}>Header 3</h2>
+    </>
+  );
 }
--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -1,8 +1,54 @@
 import { render, screen } from "@testing-library/react";
 import Header from "../Header";
 
-test("renders the header", () => {
+// GET BY
+// Passes
+test("should render the same text passed into the title prop", () => {
   render(<Header title={"My Header"} />);
   const headingElement = screen.getByText(/My Header/i);
   expect(headingElement).toBeInTheDocument();
+});
+
+// Passes - only if we specify the text of the heading we wanna target
+test("should render the H3 element", () => {
+  render(<Header />);
+  const headingElement = screen.getByRole("heading", { name: "Header 3" });
+  expect(headingElement).toBeInTheDocument();
+});
+
+// Passes - only if we specify the title of the heading we wanna target
+test("should render the H2 element", () => {
+  render(<Header title={"My Header"} />);
+  const headingElement = screen.getByTitle("Target This");
+  expect(headingElement).toBeInTheDocument();
+});
+
+// FIND BY - remember it is async
+// Passes
+test("should render the same text passed into the title prop", async () => {
+  render(<Header title={"My Header"} />);
+  const headingElement = await screen.findByText(/My Header/i);
+  expect(headingElement).toBeInTheDocument();
+});
+
+// QUERY BY
+// Passes
+test("should render the same text passed into the title prop", () => {
+  render(<Header title={"My Header"} />);
+  const headingElement = screen.queryByText(/My Header/i);
+  expect(headingElement).toBeInTheDocument();
+});
+
+// Passes - queryByText returns null and we check for the element NOT to be on the page
+test("should not display anything", () => {
+  render(<Header title={"My Header"} />);
+  const headingElement = screen.queryByText(/Dogs/i);
+  expect(headingElement).not.toBeInTheDocument();
+});
+
+// Passes
+test("should display 3 headings", () => {
+  render(<Header />);
+  const headingElements = screen.getAllByRole("heading");
+  expect(headingElements.length).toBe(3);
 });
