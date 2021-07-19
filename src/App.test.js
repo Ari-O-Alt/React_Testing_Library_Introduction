96f1bd9aecb64442f6aeee0a98895177cc000156
Arika O <11184750+Ari-O-Alt@users.noreply.github.com>
2021-07-19 21:55:01 +0200

    Added small documentation for the App.test.js failing test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+// we can use test() or it() for the test block
+// we need to tell the test what component we need to render for testing ==>   render(<App />);
+// we need to find the elements we want to interact with ==>  const linkElement = screen.getByText(/learn react/i);
+// we need to interact with those elements ==> no interaction in this specific test
+// we must assert that the results are as we expected ==> expect(linkElement).toBeInTheDocument();
