7a2907efdfb76bf60f6ae07239ab51cb9106fcb6
Arika O <11184750+Ari-O-Alt@users.noreply.github.com>
2021-07-19 22:04:45 +0200

    Added explanations on selectors

--- src/App.test.js
+++ src/App.test.js
@@ -12,3 +12,11 @@ test("renders learn react link", () => {
 // we need to find the elements we want to interact with ==>  const linkElement = screen.getByText(/learn react/i);
 // we need to interact with those elements ==> no interaction in this specific test
 // we must assert that the results are as we expected ==> expect(linkElement).toBeInTheDocument();
+
+// getBy - finds one, returns one, finds more, returns error // NO MATCH, returns error // AWAIT No
+// findBy - finds one, returns one, finds more, returns error // NO MATCH, returns error // AWAIT Yes
+// queryBy - finds one, returns one, finds more, returns error // NO MATCH, returns null // AWAIT No
+
+// getAllBy - finds one, returns array, find more, returns array // NO MATCH, returns error // AWAIT No
+// findAllBy - finds one, returns array, find more, returns array // NO MATCH, returns error // AWAIT Yes
+// queryAllBy - finds one, returns array, find more, returns array // NO MATCH, returns array // AWAIT No
