1ab991a962b17314ab701f2d9377ccb9b5197fac
Arika O <11184750+Ari-O-Alt@users.noreply.github.com>
2021-07-20 10:51:52 +0200

    Wrote test that checks for input changes

--- src/components/AddInput/__test__/AddInput.test.js
+++ src/components/AddInput/__test__/AddInput.test.js
@@ -1,11 +1,22 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import AddInput from "../AddInput";
 
 // we mock a function using Jest so we can pass it as a prop to AddInput
 const mockSetTodos = jest.fn();
 
-test("renders input element", () => {
+describe("Tests for the AddInput component", () => {
+  //passes
+  test("renders input element", () => {
+    render(<AddInput todos={[]} setTodos={mockSetTodos} />);
+    const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
+    expect(inputElement).toBeInTheDocument();
+  });
+});
+
+//passes
+test("should be able to type in the input", () => {
   render(<AddInput todos={[]} setTodos={mockSetTodos} />);
   const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
-  expect(inputElement).toBeInTheDocument();
+  fireEvent.change(inputElement, { target: { value: "Walk the dog" } });
+  expect(inputElement.value).toBe("Walk the dog");
 });
