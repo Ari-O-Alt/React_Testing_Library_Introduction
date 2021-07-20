225fa97a481cf4e23ea0cf5dc5323396683c50c7
Arika O <11184750+Ari-O-Alt@users.noreply.github.com>
2021-07-20 09:43:13 +0200

    Wrote more tests for the TodoFooter component

--- src/components/TodoFooter/__test__/TodoFooter.test.js
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -11,8 +11,15 @@ const MockTodoFooter = ({ numberOfIncompleteTasks }) => {
   );
 };
 
+// passes
 test("shows the number of incomplete tasks", () => {
   render(<MockTodoFooter numberOfIncompleteTasks={5} />);
   const paragraphElement = screen.getByText(/5 tasks left/i);
+  expect(paragraphElement).toBeInTheDocument();
+});
+
+test("should show the `task` word when the number of incomplete tasks is 1", () => {
+  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+  const paragraphElement = screen.getByText(/1 task left/i);
   expect(paragraphElement).toBeInTheDocument();
 });
