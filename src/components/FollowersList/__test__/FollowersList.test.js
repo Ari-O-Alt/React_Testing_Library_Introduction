543a922a0c3d85766eb56e9a1481eba71905e550
Arika O <11184750+Ari-O-Alt@users.noreply.github.com>
2021-07-30 20:45:41 +0200

    Wrote more tests for the followers list; used findBy

--- src/components/FollowersList/__test__/FollowersList.test.js
+++ src/components/FollowersList/__test__/FollowersList.test.js
@@ -15,4 +15,18 @@ describe("Followers list", () => {
   test("renders the followers list", () => {
     render(<MockFollowersList />);
   });
+  // passes
+  test("renders the first follower", async () => {
+    render(<MockFollowersList />);
+    // we must await this since it takes a while to retrieve the followers
+    // we must use findBy since getBy is not async
+    const followerCard = await screen.findByTestId("follower-item-0");
+    expect(followerCard).toBeInTheDocument();
+  });
+  // passes
+  test("renders all 5 followers", async () => {
+    render(<MockFollowersList />);
+    const followerCards = await screen.findAllByTestId(/follower-item-/i);
+    expect(followerCards.length).toBe(5);
+  });
 });
